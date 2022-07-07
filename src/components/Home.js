e46469a73d22604c298df888e781066850480cca
Utkarsh212 <19ucc039@lnmiit.ac.in>
2022-07-07 17:56:25 +0530

    <md space reduced

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import AnimateText from './AnimateText'
 function Home() {
   return (
     <div className='w-full h-full flex flex-col lg:flex-row justify-evenly'>
-      <div className='w-full text-white space-y-14'>
+      <div className='w-full text-white space-y-8 lg:space-y-14'>
         <span className='text-[#FFC501] text-2xl font-["La_Belle_Aurore"]'>&lt;h1&gt;</span>
         <p className='text-2xl md:text-4xl lg:text-6xl font-["Coolvetica"] tracking-wider'>
           <AnimateText text="Hi,"/><br />
@@ -13,7 +13,7 @@ function Home() {
           <AnimateText text="web" />&nbsp;<AnimateText text="developer." />&nbsp;<span className='text-[#FFC501] text-2xl font-["La_Belle_Aurore"]'>&lt;/h1&gt;</span>
         </p>
         <p className='text-gray-200 text-sm lg:text-lg tracking-widest'>FRONT END DEVELOPER</p>
-        <button type="button" className='w-[max(40%,270px)] py-2 px-4 text-[#FFC501] hover:bg-[#FFC501] border-2 border-[#FFC501] hover:text-[#354F60] w-full transition ease-in duration-200 text-center text-base font-semibold focus:outline-none focus:ring-2 focus:ring-offset-2 tracking-widest'>
+        <button type="button" className='w-[max(40%,180px)] py-2 px-4 text-[#FFC501] hover:bg-[#FFC501] border-2 border-[#FFC501] hover:text-[#354F60] w-full transition ease-in duration-200 text-center text-base font-semibold focus:outline-none focus:ring-2 focus:ring-offset-2 tracking-widest'>
           CONTACT ME
         </button>
       </div>
