030b68ed7d48a740eab75feacb677c7745ca0b03
Utkarsh212 <19ucc039@lnmiit.ac.in>
2022-07-08 20:03:44 +0530

    bottom padding added

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -10,7 +10,7 @@ function Layout() {
             <div className='w-screen min-h-screen max-h-fit lg:ml-[max(4%,71px)] lg:h-screen lg:w-full lg:p-2 bg-[#354F60] relative'>
                     <p className='ml-3 lg:mt-4 text-[#FFC501] text-2xl font-["La_Belle_Aurore"]'>&lt;html&gt;</p>
                     <p className='ml-16 mb-4 text-[#FFC501] text-2xl font-["La_Belle_Aurore"]'>&lt;body&gt;</p>
-                    <div className='w-full h-3/4 pl-24 pr-10 lg:pl-36 lg:pr-16 flex justify-center items-center'>
+                    <div className='pb-24 w-full h-3/4 pl-24 pr-10 lg:pl-36 lg:pr-16 flex justify-center items-center'>
                         <Outlet />
                     </div>
                     <p className='ml-16 text-[#FFC501] text-2xl font-["La_Belle_Aurore"] absolute bottom-12'>&lt;/body&gt;</p>
