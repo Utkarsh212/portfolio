143644875d532219c7129cd5e0f8bd484713d7ca
Utkarsh212 <19ucc039@lnmiit.ac.in>
2022-07-13 00:06:59 +0530

    data.json added

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,9 +5,9 @@ export default function Card(props) {
         <div className="h-fit w-52 m-auto overflow-hidden shadow-lg rounded-2xl md:h-90 md:w-64 hover:scale-105 ease-in-out duration-300">
             <img alt="Project" src={props.imgUrl} className="rounded-t-lg" />
             <div className="bg-[#BC0E4C] w-full h-full p-4 relative">
-                <button aria-label="Go to article" type="button" className="absolute rounded-full bg-[#FFC501] text-white w-12 h-12 right-8 -top-6 hover:scale-105 ease-in-out duration-200">
+                <a target="_blank" href={props.deploymentUrl}><button aria-label="Go to article" type="button" className="absolute rounded-full bg-[#FFC501] text-white w-12 h-12 right-8 -top-6 hover:scale-105 ease-in-out duration-200">
                 View
-                </button>
+                </button></a>
                 <p className="text-white text-lg md:text-xl font-medium mb-2 ">
                     {props.title}
                 </p>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,33 +1,29 @@
 import React from 'react'
 import AnimateText from './AnimateText'
 import Card from './Card'
+import data from '../Data/data.json'
 
 function Projects() {
-  const Username = process.env.USERNAME
-  const API_URL = `https://api.github.com/users/${Username}/repos`
-  const [projectList, setProjectList] = React.useState([])
-  const [fetchError, setFetchError] = React.useState(null)
-  const [isLoading, setIsLoading] = React.useState(true)
-  React.useEffect(() => {
+  /* const Username = process.env.USERNAME
+  const API_URL = `https://api.github.com/users/Utkarsh212/repos` */
+  const [projectList, setProjectList] = React.useState(data)
+  console.log(projectList)
+  /* React.useEffect(() => {
     const fetchRepos = async () => {
         try{
           const response = await fetch(API_URL)
           if (!response.ok) throw Error('Did not received expected data')
           const repoList = await response.json()
           setProjectList(repoList)
-          console.log(projectList)
-          setFetchError(null)
         }catch(err){
           console.log(err.message)
-          setFetchError(err.message)
-        }finally{
-          setIsLoading(false)
         }
     }
     setTimeout(()=>{
       (async () => await fetchRepos())()
     }, 2000)
-  }, [])
+  }, []) */
+
   return (
     <div className='w-full flex flex-col justify-center items-start space-y-5'>
       <div className='text-white'>
@@ -39,16 +35,16 @@ function Projects() {
         <span className='text-[#FFC501] text-xl font-["La_Belle_Aurore"]'>&lt;/h1&gt;</span>
       </div>
       <div className='min-w-fit max-w-[90%] h-[70vh] lg:min-w-[45%] lg:h-fit lg:p-5 overscroll-y-scroll overflow-y-auto space-y-8 lg:overflow-y-hidden lg:overscroll-y-none lg:grid lg:grid-flow-col lg:space-y-0 lg:space-x-5 lg:overflow-x-auto lg:overscroll-x-contain scrollbar-hide'>
-        <Card title={'PORTFOLIO'} techstack={'React, JSX, TailwindCss'} imgUrl={'https://www.tailwind-kit.com/images/car/1.jpg'}/>
-        <Card title={'PORTFOLIO'} techstack={'React, JSX, TailwindCss'} imgUrl={'https://www.tailwind-kit.com/images/car/1.jpg'}/>
-        <Card title={'PORTFOLIO'} techstack={'React, JSX, TailwindCss'} imgUrl={'https://www.tailwind-kit.com/images/car/1.jpg'}/>
-        <Card title={'PORTFOLIO'} techstack={'React, JSX, TailwindCss'} imgUrl={'https://www.tailwind-kit.com/images/car/1.jpg'}/>
-        <Card title={'PORTFOLIO'} techstack={'React, JSX, TailwindCss'} imgUrl={'https://www.tailwind-kit.com/images/car/1.jpg'}/>
-        <Card title={'PORTFOLIO'} techstack={'React, JSX, TailwindCss'} imgUrl={'https://www.tailwind-kit.com/images/car/1.jpg'}/>
-        <Card title={'PORTFOLIO'} techstack={'React, JSX, TailwindCss'} imgUrl={'https://www.tailwind-kit.com/images/car/1.jpg'}/>
-        <Card title={'PORTFOLIO'} techstack={'React, JSX, TailwindCss'} imgUrl={'https://www.tailwind-kit.com/images/car/1.jpg'}/>
-        <Card title={'PORTFOLIO'} techstack={'React, JSX, TailwindCss'} imgUrl={'https://www.tailwind-kit.com/images/car/1.jpg'}/>
-        <Card title={'PORTFOLIO'} techstack={'React, JSX, TailwindCss'} imgUrl={'https://www.tailwind-kit.com/images/car/1.jpg'}/>
+        {projectList.map((project)=>{
+          return <Card 
+            key={project.id} 
+            title={project.title} 
+            techstack={project.techstack} 
+            imgUrl={project.imgUrl} 
+            githubUrl={project.githubUrl}
+            deploymentUrl={project.deploymentUrl}
+          />
+        })}
       </div>
     </div>
   )
