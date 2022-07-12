fa992ba4251abb45408837f7a1ebc1b49aaf7fd6
Utkarsh212 <19ucc039@lnmiit.ac.in>
2022-07-12 20:40:00 +0530

    added responsiveness to projects component

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function Card(props) {
     return (
-        <div className="h-fit w-52 m-auto overflow-hidden shadow-lg rounded-2xl md:h-90 md:w-64">
+        <div className="h-fit w-52 m-auto overflow-hidden shadow-lg rounded-2xl md:h-90 md:w-64 hover:scale-105 ease-in-out duration-300">
             <img alt="Project" src={props.imgUrl} className="rounded-t-lg" />
             <div className="bg-[#BC0E4C] w-full h-full p-4 relative">
-                <button aria-label="Go to article" type="button" className="absolute rounded-full bg-[#FFC501] text-white w-12 h-12 right-8 -top-6">
+                <button aria-label="Go to article" type="button" className="absolute rounded-full bg-[#FFC501] text-white w-12 h-12 right-8 -top-6 hover:scale-105 ease-in-out duration-200">
                 View
                 </button>
                 <p className="text-white text-lg md:text-xl font-medium mb-2 ">
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,16 +4,16 @@ import Card from './Card'
 
 function Projects() {
   return (
-    <div className='w-full'>
+    <div className='w-full flex flex-col justify-center items-start space-y-5'>
       <div className='text-white'>
         <span className='text-[#FFC501] text-xl font-["La_Belle_Aurore"]'>&lt;h1&gt;</span>
         <br />
         <h1 className='text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-["League_Spartan"] font-semibold tracking-wider'>
-          <AnimateText text="Projects" />{/* &nbsp;&amp;&nbsp; */}
+          <AnimateText text="Projects" />
         </h1>
         <span className='text-[#FFC501] text-xl font-["La_Belle_Aurore"]'>&lt;/h1&gt;</span>
       </div>
-      <div className='min-w-fit max-w-[90%] max-h-screen overscroll-y-contain overflow-y-auto space-y-8'>
+      <div className='min-w-fit max-w-[90%] h-[70vh] lg:min-w-[45%] lg:h-fit lg:p-5 overscroll-y-scroll overflow-y-auto space-y-8 lg:overflow-y-hidden lg:overscroll-y-none lg:grid lg:grid-flow-col lg:space-y-0 lg:space-x-5 lg:overflow-x-auto lg:overscroll-x-contain scrollbar-hide'>
         <Card title={'PORTFOLIO'} techstack={'React, JSX, TailwindCss'} imgUrl={'https://www.tailwind-kit.com/images/car/1.jpg'}/>
         <Card title={'PORTFOLIO'} techstack={'React, JSX, TailwindCss'} imgUrl={'https://www.tailwind-kit.com/images/car/1.jpg'}/>
         <Card title={'PORTFOLIO'} techstack={'React, JSX, TailwindCss'} imgUrl={'https://www.tailwind-kit.com/images/car/1.jpg'}/>
