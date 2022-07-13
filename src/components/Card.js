a098e808d4078bf2f73cbf82137a934905cf858b
Utkarsh212 <19ucc039@lnmiit.ac.in>
2022-07-13 11:17:49 +0530

    project component completed

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,12 +3,12 @@ import React from 'react'
 export default function Card(props) {
     return (
         <div className="h-fit w-52 m-auto overflow-hidden shadow-lg rounded-2xl md:h-90 md:w-64 hover:scale-105 ease-in-out duration-300">
-            <img alt="Project" src={props.imgUrl} className="rounded-t-lg" />
+            <img alt="Project" src={props.imgUrl} className="w-full h-full rounded-t-lg" />
             <div className="bg-[#BC0E4C] w-full h-full p-4 relative">
                 <a target="_blank" href={props.deploymentUrl}><button aria-label="Go to article" type="button" className="absolute rounded-full bg-[#FFC501] text-white w-12 h-12 right-8 -top-6 hover:scale-105 ease-in-out duration-200">
                 View
                 </button></a>
-                <p className="text-white text-lg md:text-xl font-medium mb-2 ">
+                <p className="text-white text-lg md:text-xl font-medium mb-2 whitespace-nowrap">
                     {props.title}
                 </p>
                 <p className="text-white text-xs">
